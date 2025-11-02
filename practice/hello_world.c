c489b4220af00e222a3442da6dbdc3d5935cf177
Vidyarathna. B <vidyarb30@gmail.com>
2025-11-02 09:49:14 +0530

    Add practice/hello_world.c

---
+++ practice/hello_world.c
@@ -0,0 +1,11 @@
+/* practice/hello_world.c
+ * Simple C program for practice
+ * Usage: gcc practice/hello_world.c -o hello && ./hello
+ */
+
+#include <stdio.h>
+
+int main(void) {
+    printf("Hello, world!\n");
+    return 0;
+}
